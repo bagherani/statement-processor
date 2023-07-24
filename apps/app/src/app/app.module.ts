2675e6668ca08996cd97650da5eef269de1fecd8
bagherani <mohammad.bagherani@gmail.com>
2023-07-24 14:52:52 +0200

    new components added

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -6,9 +6,19 @@ import { appRoutes } from './app.routes';
 import { FileUploaderComponent } from './components/file.uploader/file.uploader.component';
 import { ResultViewComponent } from './components/result.view/result.view.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BalanceFailureComponentComponent } from './components/balance-failure-component/balance-failure-component.component';
+import { ReferenceDuplicationFailureComponentComponent } from './components/reference-duplication-failure-component/reference-duplication-failure-component.component';
+import { FileParsingFailureComponentComponent } from './components/file-parsing-failure-component/file-parsing-failure-component.component';
 
 @NgModule({
-  declarations: [AppComponent, FileUploaderComponent, ResultViewComponent],
+  declarations: [
+    AppComponent,
+    FileUploaderComponent,
+    ResultViewComponent,
+    BalanceFailureComponentComponent,
+    ReferenceDuplicationFailureComponentComponent,
+    FileParsingFailureComponentComponent,
+  ],
   imports: [
     HttpClientModule,
     BrowserModule,
