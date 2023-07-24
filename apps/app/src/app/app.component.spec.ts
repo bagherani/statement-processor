8914d310c24c434b8e641e9082790d9d965f9785
bagherani <mohammad.bagherani@gmail.com>
2023-07-24 13:47:47 +0200

    tests updated

--- apps/app/src/app/app.component.spec.ts
+++ apps/app/src/app/app.component.spec.ts
@@ -1,13 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ResultViewComponent } from './components/result.view/result.view.component';
+import { FileUploaderComponent } from './components/file.uploader/file.uploader.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent, NxWelcomeComponent],
+      imports: [RouterTestingModule, HttpClientModule],
+      declarations: [AppComponent, FileUploaderComponent, ResultViewComponent],
     }).compileComponents();
   });
 
@@ -15,12 +18,14 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome app');
+    expect(compiled.querySelector('form')?.textContent).toContain(
+      'Upload a file to validate'
+    );
   });
 
   it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Statement Validator');
   });
 });
