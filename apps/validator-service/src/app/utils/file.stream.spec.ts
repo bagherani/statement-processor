38598ef8e88170e1a83cc36ef774c40b934b7f57
bagherani <mohammad.bagherani@gmail.com>
2023-07-24 15:58:44 +0200

    test added for file-stream module

---
+++ apps/validator-service/src/app/utils/file.stream.spec.ts
@@ -0,0 +1,25 @@
+import { getFileStream } from './file.stream';
+import { Readable } from 'stream';
+
+describe('getFileStream', () => {
+  it('should return a readable stream with file buffer', () => {
+    const mockFile: Express.Multer.File = {
+      fieldname: 'file',
+      stream: null,
+      originalname: 'test.xml',
+      encoding: '7bit',
+      mimetype: 'text/xml',
+      destination: '/path/to/destination',
+      filename: 'abc123.xml',
+      path: '/path/to/destination/abc123.xml',
+      size: 1024,
+      buffer: Buffer.from('<xml>Test content</xml>'),
+    };
+
+    const result = getFileStream(mockFile);
+
+    expect(result).toBeInstanceOf(Readable);
+    expect(result.read().toString()).toBe('<xml>Test content</xml>');
+    expect(result.read()).toBeNull();
+  });
+});
