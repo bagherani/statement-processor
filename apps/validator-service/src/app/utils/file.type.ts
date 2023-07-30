1da6fc88920348e527951f4894fa36ef5b5cceb2
bagherani <mohammad.bagherani@gmail.com>
2023-07-30 13:07:12 +0200

    files renamed

--- apps/validator-service/src/app/utils/file.type.ts
+++
@@ -1,8 +0,0 @@
-import { File } from '@statement-validator/models';
-import { TransactionsFileType } from '../services/transaction.reader.factory';
-
-export function getTransactionFileType(file: File): TransactionsFileType {
-  return file.mimetype.match(/\/(xml)$/)
-    ? TransactionsFileType.xml
-    : TransactionsFileType.csv;
-}
